38d289a6251ad477b11dc79055ef6a0dca69f289
Bryan Alonso Valencia <alonsoval2.03@gmail.com>
2019-10-12 18:51:16 -0500

    docs: Se agregaron algunos comentarios.
    feat: Se agregaron algunas variables que pienso se van a necesitar.

--- Pila.py
+++ Pila.py
@@ -1,3 +1,15 @@
+# Implementación de una pila para resolver el problema:
+
+""" De escribir un método para determinar si una secuencia de caracteres de entrada es de la forma:
+	X & Y.
+
+    Siendo X una cadena de caracteres y Y la cadena inversa.
+    El carácter & es el separador. """
+
 class aplicacionDePilas:
 
-    
+    __PilaUno = ["B", "R", "Y", "A", "N", ]
+    __PilaDos = ["N", "A", "Y", "R", "B", ]
+    __PilaAux = []
+    __tamPila = int(0)
+    __cima = 0
